efe372146941e5d577a513dfdc88d64974dce10e
Barbu Petcu <barbu.petcu@gmail.com>
2025-06-01 22:46:58 +0300

    Fix serde serialization

--- penalty-points-calculator/src/main/java/com/barbu/fleetmanagement/penaltypoints/stream/PenaltyPointsTopology.java
+++ penalty-points-calculator/src/main/java/com/barbu/fleetmanagement/penaltypoints/stream/PenaltyPointsTopology.java
@@ -13,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Grouped;
--- penalty-points-calculator/src/main/java/com/barbu/fleetmanagement/penaltypoints/stream/SerdesFactory.java
+++ penalty-points-calculator/src/main/java/com/barbu/fleetmanagement/penaltypoints/stream/SerdesFactory.java
@@ -1,6 +1,7 @@
 package com.barbu.fleetmanagement.penaltypoints.stream;
 
 import com.barbu.fleetmanagement.common.model.CarPosition;
+import com.barbu.fleetmanagement.penaltypoints.domain.CarSpeed;
 import com.barbu.fleetmanagement.penaltypoints.domain.DriverPenaltyPoints;
 import io.quarkus.kafka.client.serialization.ObjectMapperSerde;
 import org.apache.kafka.common.serialization.Serde;
@@ -9,6 +10,10 @@ public class SerdesFactory {
 
     public static Serde<CarPosition> carPositionSerde() {
         return new ObjectMapperSerde<>(CarPosition.class);
+    }
+
+    public static Serde<CarSpeed> carSpeedSerde() {
+        return new ObjectMapperSerde<>(CarSpeed.class);
     }
 
     public static Serde<DriverPenaltyPoints> driverPenaltyPointsSerde() {
